9850d3269335702b479ce04e77a30e19ea5ba091
Byanka Partlow <byanka.partlow@gmail.com>
2021-08-09 08:44:22 -0700

    save stuff

---
+++ python.py
@@ -0,0 +1,20 @@
+#python3.8 file_name.py (Mac and Linux)
+#python3.8 (Mac and Linux)
+
+
+"""
+This is for 
+a long comment 
+that keeps going
+and going 
+and...
+"""
+
+
+# This is a quick note
+
+def something(athing):
+    do stuff..
+
+
+
