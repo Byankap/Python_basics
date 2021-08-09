4b886731f9ad72e1621b81bd1542416f1a5b34ba
Byanka Partlow <byanka.partlow@gmail.com>
2021-08-09 08:48:26 -0700

    playing with git

--- python.py
+++ python.py
@@ -2,19 +2,7 @@
 #python3.8 (Mac and Linux)
 
 
-"""
-This is for 
-a long comment 
-that keeps going
-and going 
-and...
-"""
 
-
-# This is a quick note
-
-def something(athing):
-    do stuff..
 
 
 
