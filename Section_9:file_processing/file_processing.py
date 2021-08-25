0b028830e9245b275ac6306a97875b906b9f6d9a
Byanka Partlow <byanka.partlow@gmail.com>
2021-08-24 21:20:27 -0700

    file_processing

--- Section_9:file_processing/file_processing.py
+++ Section_9:file_processing/file_processing.py
@@ -132,3 +132,26 @@
     file.write(content)
     file.write(content)
 
+#Review
+
+    #You can read an existing file with Python:
+
+        with open("file.txt") as file:
+            content = file.read()
+
+    #You can create a new file with Python and write some text on it:
+
+        with open("file.txt", "w") as file:
+            content = file.write("Sample text")
+
+    #You can append text to an existing file without overwriting it:
+
+        with open("file.txt", "a") as file:
+            content = file.write("More sample text")
+
+    #You can both append and read a file with:
+
+        with open("file.txt", "a+") as file:
+            content = file.write("Even more sample text")
+            file.seek(0)
+            content = file.read()
