13d9594fd90aa9a8cec60dfca443e1e49fb6ddbf
Byanka Partlow <byanka.partlow@gmail.com>
2021-09-01 21:46:43 -0700

    Theasurus

--- Section_11:Building_Thesaurus/Building_Thesaurus.py
+++ Section_11:Building_Thesaurus/Building_Thesaurus.py
@@ -1,11 +1,17 @@
 import json
+#used to read and write directly from JSON files
+import difflib
+#a library that compares texts
 
 data = json.load(open('data.json'))
 
 def translate(word):
-    return data(word)
-
-
+    word = word.lower()
+    if word in data:
+        return data[word]
+    else:
+        return "the word does not exist"
 word = input('Enter word: ')
 
-print(translate(word))
+print(translate(word)) 
+
