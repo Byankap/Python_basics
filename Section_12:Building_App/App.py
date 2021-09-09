d6163e7bd8722c8a9ca52cb4037f62e37a76f797
Byanka Partlow <byanka.partlow@gmail.com>
2021-09-09 08:50:33 -0700

    App

--- Section_12:Building_App/App.py
+++ Section_12:Building_App/App.py
@@ -5,8 +5,11 @@
 
 import mysql.connector
 
+#a library that compares text
+from difflib import get_close_matches
+
 #create a connection using a variable 
-con = mysql.connector.connect(
+con = mysql.connector.connect( #here are the credientials of database
 user = 'ardit700_student', 
 password = 'ardit700_student',
 host = '108.167.140.122',
@@ -16,10 +19,30 @@ database = 'ardit700_pm1database'
 #create a cursor object used to navigate through the table of database
 cursor = con.cursor()
 
+word = input('Enter a word: ')
+
 #create a query
-query = cursor.execute('SELECT * FROM Dictionary') #here goes sql statement
+query = cursor.execute('SELECT * FROM Dictionary WHERE Expression = "%s"' % word) #here goes sql statement
 
 #getting the data
 results = cursor.fetchall()
 
-print(results)
+#extracting the tuples from the results so it shows up as a list
+if results:
+    for result in results:
+        print(result[1]) #by adding a one, the second item(definition) is selected w/0 formatted as a tuple/str
+         
+else:
+    print('No word found, sorry')
+
+#implemented code from section 11, get_close_matches
+def dictionary (word):
+    if result in results:
+        print(result[1])
+    
+    elif len(get_close_matches(word, data.keys())) > 0
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no" %s word)
+        if yn.lower() =='y':
+            return data[get_close_matches(word,data.keys())[0]]
+        elif yn.lower() =='n':
+            return ''
