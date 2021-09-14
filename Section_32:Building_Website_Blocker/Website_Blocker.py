f07f55711deef9d7374ae85ceee84c3e47f9d14d
Byanka Partlow <byanka.partlow@gmail.com>
2021-09-14 09:30:56 -0700

    web_blocker

---
+++ Section_32:Building_Website_Blocker/Website_Blocker.py
@@ -0,0 +1,15 @@
+import time
+#check for the time of day
+from datetime import datetime as dt
+
+hosts_path = 'etc/hosts'
+redirect = '127.0.0.1'
+website_list = 'www.facebook.com', 'facebook.com', 'www.reddit.com', 'reddit.com'
+
+while True:
+    #instead of putting current year, python will generate the year for sustainability
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,14):
+        print("Working hours")
+    else:
+        print("Fun hours")
+    time.sleep(5)
